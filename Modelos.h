80251c85b5c96365245a51c3aca854aa5af36edd
jesuso <j.ochoa@norteconecta.net>
2026-06-04 15:59:02 -0400

    quitado usuario y contraseña, no los terminamos usando.

--- Modelos.h
+++ Modelos.h
@@ -49,8 +49,6 @@ struct Repartidor {
     int id;
     Cedula cedula;
     char nombre[64];
-    char usuario[32];
-    char contrasena[32];
     int id_sector; // llave foranea
     Vehiculo vehiculo;
     bool disponible;
